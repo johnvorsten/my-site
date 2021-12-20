3cfdc06332de1e49617d13e82197c42e3bcd286d
John Vorsten <vorstenjohn@gmail.com>
2021-12-19 21:30:36 -0600

    adding js to mil

---
+++ my_site/jv_blog/static/projects/mil.js
@@ -0,0 +1,20 @@
+const add_instance_elements = document.getElementsByClassName("add_instance")
+const remove_instance_elements = document.getElementsByClassName("remove_instance")
+const prediction_result_element = document.getElementById("prediction_result")
+
+add_instance_elements.addEventListener('click', add_instance)
+for (const ele of )
+remove_instance_elements.addEventListener('click', remove_instance)
+prediction_result_element.addEventListener('click', update_prediction)
+
+function add_instance() {
+
+}
+
+function remove_instance() {
+
+}
+
+function update_prediction() {
+
+}
