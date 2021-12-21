05f0beb66b0a7f38cb5c42e55b3751446801d692
John Vorsten <vorstenjohn@gmail.com>
2021-12-21 02:07:11 -0600

    work

--- my_site/jv_blog/static/projects/mil.js
+++ my_site/jv_blog/static/projects/mil.js
@@ -1,12 +1,21 @@
+// Imports
+
+// Variable declaration
 const add_instance_elements = document.getElementsByClassName("add_instance")
-const remove_instance_elements = document.getElementsByClassName("remove_instance")
+let remove_instance_elements = document.getElementsByClassName("remove_instance")
 const prediction_result_element = document.getElementById("prediction_result")
 
+// Main
+for (const ele of add_instance_elements) {
+    ele.addEventListener('click', add_instance)
+}
 add_instance_elements.addEventListener('click', add_instance)
-for (const ele of )
-remove_instance_elements.addEventListener('click', remove_instance)
+for (const ele of remove_instance_elements) {
+    ele.addEventListener('click', remove_instance)
+}
 prediction_result_element.addEventListener('click', update_prediction)
 
+// functions
 function add_instance() {
 
 }
@@ -16,5 +25,5 @@ function remove_instance() {
 }
 
 function update_prediction() {
-
+    alert("Updated prediction")
 }
