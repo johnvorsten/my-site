b81ca0ff8895af045b9fdbd58386e9c28e786ca4
John Vorsten <vorstenjohn@gmail.com>
2021-12-22 09:25:59 -0600

    work

--- my_site/jv_blog/static/projects/mil.js
+++ my_site/jv_blog/static/projects/mil.js
@@ -1,29 +1,48 @@
 // Imports
 
 // Variable declaration
-const add_instance_elements = document.getElementsByClassName("add_instance")
-let remove_instance_elements = document.getElementsByClassName("remove_instance")
-const prediction_result_element = document.getElementById("prediction_result")
+const add_instance_elements = document.getElementsByClassName("add_instance");
+let remove_instance_elements = document.getElementsByClassName("remove_instance");
+const run_prediction_element = document.getElementById("run_prediction");
 
-// Main
+// Events
 for (const ele of add_instance_elements) {
-    ele.addEventListener('click', add_instance)
+    ele.addEventListener('click', add_instance);
 }
-add_instance_elements.addEventListener('click', add_instance)
 for (const ele of remove_instance_elements) {
-    ele.addEventListener('click', remove_instance)
+    ele.addEventListener('click', remove_instance);
 }
-prediction_result_element.addEventListener('click', update_prediction)
+run_prediction_element.addEventListener('click', run_prediction);
 
 // functions
 function add_instance() {
-
+    console.log("Adding instance");
 }
 
 function remove_instance() {
-
+    // Find element ID
+    // Remove element
+    console.log("Removing instance");
 }
 
-function update_prediction() {
-    alert("Updated prediction")
+function run_prediction() {
+    // Run prediction API call
+    // Validate data
+    // Aggregate instances and format data
+    // Call API
+    let prediction = "Some text temporary"
+    // Return result
+    update_prediction_text(prediction);
+}
+
+function update_prediction_text(prediction) {
+    // Change text in prediction_result
+    const run_prediction_element = document.getElementById("prediction_result");
+    run_prediction_element.textContent = prediction;
+    // TODO remove
+    console.log("Updated prediction");
+}
+
+function gather_instances() {
+    // aggregate all instances with class 'tr' (table row) into a JSON string for use in API header
 }
