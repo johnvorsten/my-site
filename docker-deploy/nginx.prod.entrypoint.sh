9b972bb4f40da7c44bd4dae7e671e943dd7104f4
John Vorsten <vorstenjohn@gmail.com>
2021-12-29 09:05:14 -0600

    Nginx configuration files proxy pass MIL predict Modify Nginx configuration files for prediction requests from javascript to MIL prediction server

--- docker-deploy/nginx.prod.entrypoint.sh
+++ docker-deploy/nginx.prod.entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Create the default Nginx configuration file with a self-signed certificate
-envsubst '${WEBAPP_HOSTNAME},${WEBAPP_INTERNAL_PORT},${REVERSE_PROXY_HOSTNAME},${HOME}' < /etc/nginx/conf.d/nginx.test.template > /etc/nginx/conf.d/localhost.conf
+envsubst '${WEBAPP_HOSTNAME},${WEBAPP_INTERNAL_PORT},${REVERSE_PROXY_HOSTNAME},${HOME}' < /etc/nginx/conf.d/nginx.prod.template > /etc/nginx/conf.d/johnvorstne.me.conf
 
 # Debugging
 if $DEBUG; then
-    cat /etc/nginx/conf.d/localhost.conf
+    cat /etc/nginx/conf.d/johnvorsten.me.conf
 fi
 
 # Start nginx
