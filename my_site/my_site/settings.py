1d457230a448c255053ace75eced8561c26ffc0e
John Vorsten <vorstenjohn@gmail.com>
2022-01-13 21:18:17 -0600

    work

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE','django.db.backends.sqlite3'),
-        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.environ.get('SQL_USER','root'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD','root'),
-        'HOST': os.environ.get('SQL_HOST', 'localhost'),
-        'PORT': os.environ.get('SQL_PORT', '1433'),
+        'ENGINE': os.environ.get('SQL_ENGINE'),
+        'NAME': os.environ.get('SQL_DATABASE'),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT'),
         'CONN_MAX_AGE': 10,
     }
 }
