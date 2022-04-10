d3c628a68723c23eb5c497f863868325c84b783f
John Vorsten <vorstenjohn@gmail.com>
2022-04-10 07:34:52 -0500

    Delete Projects app

--- my_site/projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'projects'
-urlpatterns = [
-    path('', view=views.IndexView.as_view(), name='index'), # landing page
-    path('<slug:slug>/', view=views.EntryDetail.as_view(), name='entry_detail'), # Match an article name
-]
