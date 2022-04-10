661c1be772665191c4337c3ab452e35b58f2f84c
John Vorsten <vorstenjohn@gmail.com>
2022-04-10 07:35:30 -0500

    Delete jv_github app

--- my_site/jv_github/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JvGithubConfig(AppConfig):
-    name = 'jv_github'
--- my_site/jv_github/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'jv_github'
-urlpatterns = [
-    path('', views.IndexView.as_view(), {'github_username':'johnvorsten'}, name='index'), # landing page
-]
